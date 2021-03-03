cbd96bddb0a9e1fb002d569b3c8dcc3cf38f9202
Joan J <joanne.javillo@gmail.com>
2021-03-03 22:14:05 +1030

    initial readMe

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # note-taker
+![License](https://img.shields.io/badge/License-MIT-purple)
+
+# Description 
+This application will enable user to write and save notes
+---
+# Table of Contents 
+
+  - [Description](#Description)
+  - [Technologies](#Technologies)
+  - [Links](#Links
+  - [License](#License)
+
+---
+
+## Technologies
+node js, npm, express, JSON, Heroku
+
+---
+
+## Links
+
+## License
+MIT
--- server.js
+++ server.js
@@ -27,9 +27,16 @@ const notes = [];
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,'public', 'index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname,'public', 'notes.html')));
 
-//Post
+//Read
 app.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname,'db', 'db.json'))
+    fs.readFile(path.join(__dirname,'db', 'db.json', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const notes = JSON.parse(data.toString());
+
+        console.log(" ");
+    }))
     
 })
 
@@ -37,6 +44,8 @@ app.get('/api/notes', (req, res) => {
 app.post('/api/notes', (req, res) => {
 
 })
+
+//Read json file
 
 
 
